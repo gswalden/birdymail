21ec0adc84f64ae196888ad9a5ae7f67b7623616
gswalden <gswalden@yahoo.com>
2013-03-01 00:21:13 -0700

    working (end of thurs)

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-// BEGIN MailParser
-require_once('MimeMailParser.class.php');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
 
-$path = 'path/to/mail.txt';
+// BEGIN MailParser
+require_once('classes/MimeMailParser.php');
+
+$path = '/var/www/gswalden.com/mailhawk/classes/mail.txt';
 $Parser = new MimeMailParser();
 $Parser->setPath($path);
 
@@ -13,16 +16,6 @@ $text = $Parser->getMessageBody('text');
 $html = $Parser->getMessageBody('html');
 $attachments = $Parser->getAttachments();
 // END MailParser
-
-// BEGIN Codebird
-require_once ('php/codebird.php');
-Codebird::setConsumerKey('WY5DGrh9ipWj8UngDdhXJg', 'zTDypsqR3dJ7rxlA2CywvbfMCoXoZaUc0Ky1H8ulI'); // static, see 'Using multiple Codebird instances'
-
-$cb = Codebird::getInstance();
-$cb->setToken('1227070020-znV3OhwIxitVmLlDZkeNJen7NQWzHqmlzXfUv81', 'pNdPvydRtAPVIBNT7K7FwHVWNBhW2eZCkRJRxHq0');
-
-$reply = $cb->statuses_update("status=@Username --$subject-- link");
-// END Codebird
 ?>
 
 <!doctype html>
---
+++ oauth/app_tokens.php
@@ -0,0 +1,11 @@
+<?php
+
+// OAuth tokens for single-user programming
+// After you create an app you MUST copy 
+// the tokens and paste them into the following lines
+$consumer_key = 'WY5DGrh9ipWj8UngDdhXJg';
+$consumer_secret = 'zTDypsqR3dJ7rxlA2CywvbfMCoXoZaUc0Ky1H8ulI';
+$user_token = '1227070020-4NoDjwWBFp4SpbxqWrMB8Xl9qnriLA6SQHw25UX';
+$user_secret = 'OwsF8cLnDhjfvHWYbG7yMfWXEZAjnVePswSf5VAKwQs';
+
+?>
