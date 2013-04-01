931a7d6dbaa43dfdc3858c7d11c4aae9070c2f05
gswalden <gswalden@yahoo.com>
2013-04-01 18:30:48 -0400

    moved db to innodb, removed redundant operations

--- parser/expire.php
+++ parser/expire.php
@@ -16,10 +16,8 @@ try {
 	$stmt_get->execute();
 	if ($stmt_get->rowCount() > 0)
 		while ($id = $stmt_get->fetch()):
-			$stmt_move = $db->prepare('INSERT INTO inactive (id, sender, subject, textbody, htmlbody, date) (SELECT id, sender, subject, textbody, htmlbody, date FROM active WHERE id = :id)');
-			$stmt_move->execute(array(':id' => $id[0]));
-			if ($stmt_move->rowCount() > 0)
-				$db->prepare('DELETE FROM active WHERE id = :id')->execute(array(':id' => $id[0]));
+			$db->prepare('INSERT INTO inactive (id, sender, subject, textbody, htmlbody, date) (SELECT id, sender, subject, textbody, htmlbody, date FROM active WHERE id = :id)')
+			   ->execute(array(':id' => $id[0]));
 			$db->prepare('DELETE FROM users WHERE id = :id')->execute(array(':id' => $id[0]));
 		endwhile;
 } catch(PDOException $ex) {
